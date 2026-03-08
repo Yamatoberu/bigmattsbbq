2c37ad9ece1a241828018f95d92c0424595472b6
mgregory <gregory6267@gmail.com>
2026-03-08 10:56:13 -0600

    Build Errors 3

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -2,6 +2,8 @@ import { CheckoutClient } from "../../components/CheckoutClient";
 import { NavBar } from "../../components/NavBar";
 import { getSquareEnv } from "../../lib/env";
 
+export const dynamic = "force-dynamic";
+
 export default function CheckoutPage() {
   const env = getSquareEnv();
   return (
